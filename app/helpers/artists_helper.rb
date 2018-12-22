56d456a75615d822ac6bef62d65125bbeeb9fefc
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-22 19:51:13 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,7 +1,7 @@
 module ArtistsHelper
   def display_artist(song)
     if song.artist.nil?
-      concat(render(:partial =>"artists/link", locals:{name:"Add Artist",link:edit_song_path(song)})) 
+      concat(render(:partial =>"artists/link", locals:{name:"Add Artist",link:edit_song_path(song)}))
     else
       concat(render(:partial =>"artists/link", locals:{name:song.artist_name,link:artist_path(song.artist)}))
     end
