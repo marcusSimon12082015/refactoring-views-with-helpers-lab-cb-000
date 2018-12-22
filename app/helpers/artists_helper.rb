370017de002621bee9d504590a42c2f1b9b3443b
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-12-22 20:10:22 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+  def display_artist(song)
+    @html = ""
+    if song.artist.nil?
+      @html = concat(render(:partial =>"artists/link", locals:{name:"Add Artist",link:edit_song_path(song)}))
+    else
+      @html = concat(render(:partial =>"artists/link", locals:{name:song.artist_name,link:artist_path(song.artist)}))
+    end
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    @artist = Artist.find_by(name: name)
+    if @artist.nil?
+      self.artist = Artist.new(name: name)
+    else
+      self.artist = @artist
+    end
   end
 end
